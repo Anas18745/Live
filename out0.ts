dcfb5c41b65628c2d7c9b30003d574a6675a0ab6
Anas18745 <mohammedanas.9008@gmail.com>
2024-08-24 17:21:13 +0530

    Add files via upload

---
+++ out0.ts
@@ -0,0 +1,14 @@
+#EXTM3U
+#EXT-X-TARGETDURATION:8
+#EXT-X-VERSION:10
+#EXT-X-MEDIA-SEQUENCE:21457
+#EXT-X-ALLOW-CACHE:YES
+#EXT-X-PROGRAM-DATE-TIME:2023-06-07T03:56:28Z
+#EXTINF:7.920,
+http://tv.cloudcdn.me:80/live.ts?channelId=275&uid=9246&deviceMac=00:1A:79:1F:2E:5A
+#EXTINF:8.000
+http://tv.cloudcdn.me:80/live.ts?channelId=275&uid=9246&deviceMac=00:1A:79:1F:2E:5A
+#EXTINF:8.000
+http://tv.cloudcdn.me:80/live.ts?channelId=275&uid=9246&deviceMac=00:1A:79:1F:2E:5A
+#EXTINF:8.000
+http://tv.cloudcdn.me:80/live.ts?channelId=275&uid=9246&deviceMac=00:1A:79:1F:2E:5A
