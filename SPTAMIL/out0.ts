b8afec00cefd786a75e889b468271a8522dc796a
Anas18745 <mohammedanas.9008@gmail.com>
2024-09-01 18:37:35 +0530

    Add files via upload

--- SPTAMIL/out0.ts
+++ SPTAMIL/out0.ts
@@ -2,6 +2,6 @@
 #EXT-X-TARGETDURATION:10
 #EXT-X-VERSION:3
 #EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://tv.cloudcdn.me:80/live.ts?channelId=275&uid=9246&deviceMac=00:1A:79:1F:2E:5A
+http://tv.cloudcdn.me:80/live.ts?channelId=275&uid=1960&deviceMac=00:1A:79:4C:95:D3
 #EXTINF:10, BANDWIDTH=3000000, FRAME-RATE=60.0, AUDIO="5.1", CODECS="avc1.640028,ec-3.1.1,mp4a.40.5",
-http://tv.cloudcdn.me:80/live.ts?channelId=275&uid=9246&deviceMac=00:1A:79:1F:2E:5A
+http://tv.cloudcdn.me:80/live.ts?channelId=275&uid=1960&deviceMac=00:1A:79:4C:95:D3
